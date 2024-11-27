8b6222ac8fb1a265d3b6fdb7288a4ebef38b2d96
Sampath Surineni <sampath.surineni@gmail.com>
2024-11-27 23:26:56 +0530

    added weater styling

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -1,3 +1,4 @@
+import "./CurrentWeather.css";
 export default function CurrentWeather() {
     return (
         <div className="weather">
@@ -5,7 +6,27 @@ export default function CurrentWeather() {
                 <p className="city">Nizamabad</p>
                 <p className="weather-description">Cloudy</p>
             </div>
-            <img alt="weather" className="weather-icon" src="http://openweathermap.org/img/wn/04n.png" />
+            <img alt="weather" className="weather-icon" src="icons/01d.png" />
+            <div className="bottom">
+                <p className="temperature">31°C</p>
+                <div className="details">
+                    <p className="parameter-row">
+                        <span className="parameter-label">Details</span>
+                    </p>
+                    <p className="parameter-row">
+                        <span className="parameter-label">Feels like</span>
+                        <span className="parameter-value">30°C</span>
+                    </p>
+                    <p className="parameter-row">
+                        <span className="parameter-label">Wind</span>
+                        <span className="parameter-value">11 km/h</span>
+                    </p>
+                    <p className="parameter-row">
+                        <span className="parameter-label">Humidity</span>
+                        <span className="parameter-value">60%</span>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
