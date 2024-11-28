df859d45d0dbfe487e1bbd0d974f8dcd1f717363
ssurineni <ssurineni@salesforce.com>
2024-11-28 17:23:14 +0530

    added css to current weater component

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -3,28 +3,34 @@ export default function CurrentWeather() {
     return (
         <div className="weather">
             <div className="top">
-                <p className="city">Nizamabad</p>
-                <p className="weather-description">Cloudy</p>
+                <div>
+                    <p className="city">Nizamabad</p>
+                    <p className="weather-description">Cloudy</p>
+                </div>
+                <img alt="weather" className="weather-icon" src="icons/01d.png" />
             </div>
-            <img alt="weather" className="weather-icon" src="icons/01d.png" />
             <div className="bottom">
                 <p className="temperature">31°C</p>
                 <div className="details">
-                    <p className="parameter-row">
-                        <span className="parameter-label">Details</span>
-                    </p>
-                    <p className="parameter-row">
+                    <div className="parameter-row">
+                        <span className="parameter-label top">Details</span>
+                    </div>
+                    <div className="parameter-row">
                         <span className="parameter-label">Feels like</span>
-                        <span className="parameter-value">30°C</span>
-                    </p>
-                    <p className="parameter-row">
+                        <span className="parameter-value">31°C</span>
+                    </div>
+                    <div className="parameter-row">
                         <span className="parameter-label">Wind</span>
                         <span className="parameter-value">11 km/h</span>
-                    </p>
-                    <p className="parameter-row">
+                    </div>
+                    <div className="parameter-row">
                         <span className="parameter-label">Humidity</span>
-                        <span className="parameter-value">60%</span>
-                    </p>
+                        <span className="parameter-value">64%</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Pressure</span>
+                        <span className="parameter-value">1023 hPa</span>
+                    </div>
                 </div>
             </div>
         </div>
