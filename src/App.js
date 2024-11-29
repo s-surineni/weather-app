ef4db5848c62c5bc4a1a12e31fb5889ab0872197
ssurineni <ssurineni@salesforce.com>
2024-11-29 12:08:52 +0530

    calling apis parallelly

--- src/App.js
+++ src/App.js
@@ -9,17 +9,38 @@ function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [forecast, setForecast] = useState(null);
   const [dump, setDump] = useState("");
+  const resolveProms1 = async (dayProm, forecastProm) => {
+    await dayProm;
+    await forecastProm;
+    dayProm.then(async (response) => {
+      setWeatherData(await response.json());
+    })
+    forecastProm.then(async (response2) => {
+      setForecast(await response2.json());
+    })
+  }
+
+  const resolveProms2 = async (dayProm, forecastProm) => {
+    await Promise.all([dayProm, forecastProm]).then(async (responses) => {
+      setWeatherData(await responses[0].json());
+      setForecast(await responses[1].json());
+    })
+  }
+
+
   const handleCitySelection = async (city) => {
     setDump(JSON.stringify(city));
     const [lat, lon] = city.value.split(" ");
     try {
-      const response = await (await fetch(`${WEATHER_API_URL}/city/latlon/${lat}/${lon}&units=metric`, WEATHER_API_OPTIONS)).json();
-      setDump(JSON.stringify( weatherData));
-      setWeatherData(response);
-      const response2 = await (await fetch(`${WEATHER_API_URL}/city/fivedaysforcast/${lat}/${lon}?units=metric`, WEATHER_API_OPTIONS)).json();
-      setForecast(response2);
-      setDump(JSON.stringify(response2));
-      console.log('ironman response2', JSON.stringify(response2));
+      const dayProm = fetch(`${WEATHER_API_URL}/city/latlon/${lat}/${lon}&units=metric`, WEATHER_API_OPTIONS);
+      // setWeatherData(response);
+      const forecastProm = fetch(`${WEATHER_API_URL}/city/fivedaysforcast/${lat}/${lon}?units=metric`, WEATHER_API_OPTIONS);
+
+
+      console.time('resolveProms2');
+      resolveProms2(dayProm, forecastProm);
+      console.timeEnd('resolveProms2');
+
     } catch (error) {
       console.error(error);
     }
